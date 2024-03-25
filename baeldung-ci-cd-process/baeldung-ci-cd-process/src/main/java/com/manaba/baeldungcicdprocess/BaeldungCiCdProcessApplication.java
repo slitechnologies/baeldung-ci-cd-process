07cd9b73e5e3b8fe5875fb8a0438ce56e2104384
Tinodashe kayenie <90652443+slitechnologies@users.noreply.github.com>
2024-03-25 16:34:29 +0200

    First Commit

---
+++ baeldung-ci-cd-process/baeldung-ci-cd-process/src/main/java/com/manaba/baeldungcicdprocess/BaeldungCiCdProcessApplication.java
@@ -0,0 +1,13 @@
+package com.manaba.baeldungcicdprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeldungCiCdProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeldungCiCdProcessApplication.class, args);
+	}
+
+}
---
+++ baeldung-ci-cd-process/baeldung-ci-cd-process/src/test/java/com/manaba/baeldungcicdprocess/BaeldungCiCdProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manaba.baeldungcicdprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeldungCiCdProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
